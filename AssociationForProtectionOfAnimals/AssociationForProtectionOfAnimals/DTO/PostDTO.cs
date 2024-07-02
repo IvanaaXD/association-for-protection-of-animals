892135ca657d1d2a65a754738775565467a01c56
Ivana_R <ivanar1357@gmail.com>
2024-07-02 15:13:02 +0200

    add post class and post dto

---
+++ AssociationForProtectionOfAnimals/AssociationForProtectionOfAnimals/DTO/PostDTO.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using AssociationForProtectionOfAnimals.Domain.Model;
+using AssociationForProtectionOfAnimals.Domain.Model.Enums;
+
+namespace AssociationForProtectionOfAnimals.DTO
+{
+    public class PostDTO : INotifyPropertyChanged, IDataErrorInfo
+    {
+        private int id;
+        private DateTime dateOfPosting;
+        private DateTime dateOfUpdating;
+        private PostStatus postStatus;
+        private bool hasCurrentAdopter;
+        private int animalId;
+        private string person;
+        private string adopter;
+
+        private string adopterName;
+        private string personName;
+        private Breed animalBreed;
+        private string animalName;
+
+        public int Id
+        {
+            get { return id; }
+            set { SetProperty(ref id, value); }
+        }
+
+        public DateTime DateOfPosting
+        {
+            get { return dateOfPosting; }
+            set { SetProperty(ref dateOfPosting, value); }
+        }
+
+        public DateTime DateOfUpdating
+        {
+            get { return dateOfUpdating; }
+            set { SetProperty(ref dateOfUpdating, value); }
+        }
+
+        public PostStatus PostStatus
+        {
+            get { return postStatus; }
+            set { SetProperty(ref postStatus, value); }
+        }
+
+        public bool HasCurrentAdopter
+        {
+            get { return hasCurrentAdopter; }
+            set { SetProperty(ref hasCurrentAdopter, value); }
+        }
+
+        public int AnimalId
+        {
+            get { return animalId; }
+            set { SetProperty(ref animalId, value); }
+        }
+
+        public string Person
+        {
+            get { return person; }
+            set { SetProperty(ref person, value); }
+        }
+
+        public string Adopter
+        {
+            get { return adopter; }
+            set { SetProperty(ref adopter, value); }
+        }
+
+        public string AdopterName
+        {
+            get { return adopterName; }
+            set { SetProperty(ref adopterName, value); }
+        }
+
+        public string PersonName
+        {
+            get { return personName; }
+            set { SetProperty(ref personName, value); }
+        }
+
+        public Breed AnimalBreed
+        {
+            get { return animalBreed; }
+            set { SetProperty(ref animalBreed, value); }
+        }
+
+        public string AnimalName
+        {
+            get { return animalName; }
+            set { SetProperty(ref animalName, value); }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string? propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(storage, value))
+            {
+                return false;
+            }
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        public string Error => null;
+
+        public string this[string columnName]
+        {
+            get
+            {
+                // Add any property-specific validation logic here
+                switch (columnName)
+                {
+                    // Example validation
+                    // case "AnimalName":
+                    //     if (string.IsNullOrWhiteSpace(animalName))
+                    //         return "Animal name cannot be empty";
+                    //     break;
+                    default:
+                        return null;
+                }
+            }
+        }
+
+        public Post ToPost()
+        {
+            return new Post
+            {
+                Id = id,
+                DateOfPosting = dateOfPosting,
+                DateOfUpdating = dateOfUpdating,
+                PostStatus = postStatus,
+                HasCurrentAdopter = hasCurrentAdopter,
+                AnimalId = animalId,
+                Person = person,
+                Adopter = adopter
+            };
+        }
+
+        public PostDTO() { }
+
+        public PostDTO(Post post)
+        {
+            id = post.Id;
+            dateOfPosting = post.DateOfPosting;
+            dateOfUpdating = post.DateOfUpdating;
+            postStatus = post.PostStatus;
+            hasCurrentAdopter = post.HasCurrentAdopter;
+            animalId = post.AnimalId;
+            person = post.Person;
+            adopter = post.Adopter;
+
+            /*adopterName = post.AdopterName;
+            personName = post.PersonName;
+            animalBreed = post.AnimalBreed;
+            animalName = post.AnimalName;*/
+        }
+    }
+}
---
+++ AssociationForProtectionOfAnimals/AssociationForProtectionOfAnimals/Domain/Model/Post.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using AssociationForProtectionOfAnimals.Domain.Model.Enums;
+using AssociationForProtectionOfAnimals.Storage.Serialization;
+
+namespace AssociationForProtectionOfAnimals.Domain.Model
+{
+    public class Post : ISerializable
+    {
+        private int id;
+        private DateTime dateOfPosting;
+        private DateTime dateOfUpdating;
+        private PostStatus postStatus;
+        private bool hasCurrentAdopter;
+        private int animalId;
+        private string person;
+        private string adopter;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public DateTime DateOfPosting
+        {
+            get { return dateOfPosting; }
+            set { dateOfPosting = value; }
+        }
+
+        public DateTime DateOfUpdating
+        {
+            get { return dateOfUpdating; }
+            set { dateOfUpdating = value; }
+        }
+
+        public PostStatus PostStatus
+        {
+            get { return postStatus; }
+            set { postStatus = value; }
+        }
+
+        public bool HasCurrentAdopter
+        {
+            get { return hasCurrentAdopter; }
+            set { hasCurrentAdopter = value; }
+        }
+
+        public int AnimalId
+        {
+            get { return animalId; }
+            set { animalId = value; }
+        }
+
+        public string Person
+        {
+            get { return person; }
+            set { person = value; }
+        }
+
+        public string Adopter
+        {
+            get { return adopter; }
+            set { adopter = value; }
+        }
+
+        public Post() { }
+
+        public Post(int id, DateTime dateOfPosting, DateTime dateOfUpdating, PostStatus postStatus, bool hasCurrentAdopter, int animalId, string person, string adopter)
+        {
+            this.id = id;
+            this.dateOfPosting = dateOfPosting;
+            this.dateOfUpdating = dateOfUpdating;
+            this.postStatus = postStatus;
+            this.hasCurrentAdopter = hasCurrentAdopter;
+            this.animalId = animalId;
+            this.person = person;
+            this.adopter = adopter;
+        }
+
+        public string[] ToCSV()
+        {
+            string[] csvValues =
+            {
+                Id.ToString(),
+                DateOfPosting.ToString("yyyy-MM-dd"),
+                DateOfUpdating.ToString("yyyy-MM-dd"),
+                PostStatus.ToString(),
+                HasCurrentAdopter.ToString(),
+                AnimalId.ToString(),
+                Person.ToString(),
+                Adopter.ToString()
+            };
+            return csvValues;
+        }
+
+        public void FromCSV(string[] values)
+        {
+            if (values.Length != 8)
+                throw new ArgumentException("Invalid number of post values in CSV");
+
+            id = int.Parse(values[0]);
+            dateOfPosting = DateTime.ParseExact(values[1], "yyyy-MM-dd", null);
+            dateOfUpdating = DateTime.ParseExact(values[2], "yyyy-MM-dd", null);
+            postStatus = (PostStatus)Enum.Parse(typeof(PostStatus), values[3]);
+            hasCurrentAdopter = bool.Parse(values[4]);
+            animalId = int.Parse(values[5]); 
+            person = values[6];
+            adopter = values[7];
+        }
+    }
+}
