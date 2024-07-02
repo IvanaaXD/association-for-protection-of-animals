c38d4c2f8b08d0301ef1222ff24bd2c0d4e1ac35
Milica Stanojlovic <mstanojlovic222@gmail.com>
2024-07-02 20:21:52 +0200

    added request classes

---
+++ AssociationForProtectionOfAnimals/AssociationForProtectionOfAnimals/Domain/Model/Request.cs
@@ -0,0 +1,21 @@
+﻿using AssociationForProtectionOfAnimals.Domain.Model.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssociationForProtectionOfAnimals.Domain.Model
+{
+    public class Request
+    {
+        public RequestStatus Status { get; set; }
+        public DateTime RequestSubmissionDate { get; set; }
+
+        public Request(RequestStatus status, DateTime requestSubmissionDate)
+        {
+            Status = status;
+            RequestSubmissionDate = requestSubmissionDate;
+        }
+    }
+}
---
+++ AssociationForProtectionOfAnimals/AssociationForProtectionOfAnimals/Domain/Model/TemporaryShelterRequest.cs
@@ -0,0 +1,22 @@
+﻿using AssociationForProtectionOfAnimals.Domain.Model.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssociationForProtectionOfAnimals.Domain.Model
+{
+    public class TemporaryShelterRequest : Request
+    {
+        public DateTime AccommodationDate { get; set; }
+        public DateTime ReturnDate { get; set; }
+
+        public TemporaryShelterRequest(RequestStatus status, DateTime requestSubmissionDate, DateTime accommodationDate, DateTime returnDate)
+            : base(status, requestSubmissionDate)
+        {
+            AccommodationDate = accommodationDate;
+            ReturnDate = returnDate;
+        }
+    }
+}
