28184c20c56c6fc38bf0054185b446c8236c55d2
Milica Stanojlovic <mstanojlovic222@gmail.com>
2024-07-03 18:02:46 +0200

    add tables for req

--- AssociationForProtectionOfAnimals/AssociationForProtectionOfAnimals/Domain/Model/TemporaryShelterRequest.cs
+++ AssociationForProtectionOfAnimals/AssociationForProtectionOfAnimals/Domain/Model/TemporaryShelterRequest.cs
@@ -1,6 +1,7 @@
 ﻿using AssociationForProtectionOfAnimals.Domain.Model.Enums;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,14 +10,46 @@ namespace AssociationForProtectionOfAnimals.Domain.Model
 {
     public class TemporaryShelterRequest : Request
     {
+        public DateTime accommodationDate;
+        public DateTime returnDate;
         public DateTime AccommodationDate { get; set; }
         public DateTime ReturnDate { get; set; }
-
+        public TemporaryShelterRequest(int id,int registeredUserId, int volunteerId, RequestStatus status, DateTime requestSubmissionDate, DateTime accommodationDate, DateTime returnDate)
+            : base(id, registeredUserId, volunteerId, status, requestSubmissionDate)
+        {
+            this.accommodationDate = accommodationDate;
+            this.returnDate = returnDate;
+        }
         public TemporaryShelterRequest(int registeredUserId, int volunteerId, RequestStatus status, DateTime requestSubmissionDate, DateTime accommodationDate, DateTime returnDate)
             : base(registeredUserId, volunteerId, status, requestSubmissionDate)
         {
             AccommodationDate = accommodationDate;
             ReturnDate = returnDate;
+        }
+        public string[] ToCSV()
+        {
+            string[] csvValues =
+            {
+                id.ToString(),
+                volunteerId.ToString(),
+                registeredUserId.ToString(),
+                requestStatus.ToString(),
+                requestSubmissionDate.ToString("yyyy-MM-dd"),
+                AccommodationDate.ToString("yyyy-MM-dd"),
+                ReturnDate.ToString("yyyy-MM-dd")
+            };
+            return csvValues;
+        }
+
+        public void FromCSV(string[] csvValues)
+        {
+            int id = int.Parse(csvValues[0]);
+            int volunteerId = int.Parse(csvValues[1]);
+            int registeredUserId = int.Parse(csvValues[2]);
+            RequestStatus requestStatus = (RequestStatus)Enum.Parse(typeof(RequestStatus), csvValues[3]);
+            DateTime requestSubmissionDate = DateTime.ParseExact(csvValues[4], "yyyy-MM-dd", CultureInfo.InvariantCulture);
+            DateTime accommodationDate = DateTime.ParseExact(csvValues[5], "yyyy-MM-dd", CultureInfo.InvariantCulture);
+            DateTime returnDate = DateTime.ParseExact(csvValues[6], "yyyy-MM-dd", CultureInfo.InvariantCulture);
         }
     }
 }
---
+++ AssociationForProtectionOfAnimals/AssociationForProtectionOfAnimals/View/Adoption/VolunteerRequests.xaml.cs
@@ -0,0 +1,145 @@
+﻿
+using AssociationForProtectionOfAnimals.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using AssociationForProtectionOfAnimals.DTO;
+using System.Collections.ObjectModel;
+using AssociationForProtectionOfAnimals.Observer;
+using AssociationForProtectionOfAnimals.Controller;
+using AssociationForProtectionOfAnimals.Domain.IUtility;
+using AssociationForProtectionOfAnimals.Domain.Utility;
+using AssociationForProtectionOfAnimals.Domain.Model.Enums;
+using System.Windows.Controls;
+using System.Windows.Input;
+namespace AssociationForProtectionOfAnimals.View.Adoption
+{
+    /// <summary>
+    /// Interaction logic for VolunteerRequests.xaml
+    /// </summary>
+    public partial class VolunteerRequests: Window, IObserver
+    {
+        
+        public class ViewModel
+        {
+            public ObservableCollection<AdoptionRequestDTO> AdoptionRequestDTOs { get; set; }
+            public ObservableCollection<TemporaryShelterRequestDTO> TempShelterRequestDTOs { get; set; }
+
+            public ViewModel()
+            {
+                AdoptionRequestDTOs = new ObservableCollection<AdoptionRequestDTO>();
+                TempShelterRequestDTOs = new ObservableCollection<TemporaryShelterRequestDTO>();
+            }
+
+        }
+
+        public ViewModel TableViewModel { get; set; }
+        public AnimalDTO SelectedAnimal { get; set; }
+        private RegisteredUserController regUserController { get; set; }
+        private VolunteerController volunteerController { get; set; }
+        private PostController postController { get; set; }
+
+        private RequestController requestController { get; set; }
+
+        private int userId { get; set; }
+        private bool isSearchButtonClicked = false;
+
+        public VolunteerRequests(int userId)
+        {
+            InitializeComponent();
+            TableViewModel = new ViewModel();
+            regUserController = Injector.CreateInstance<RegisteredUserController>();
+            volunteerController = Injector.CreateInstance<VolunteerController>();
+            postController = Injector.CreateInstance<PostController>();
+            requestController = Injector.CreateInstance<RequestController>();   
+
+            this.userId = userId;
+
+
+            DataContext = this;
+            //teacherController.Subscribe(this);
+            Update();
+        }
+
+        public void Update()
+        {
+            try
+            {
+                TableViewModel.AdoptionRequestDTOs.Clear();
+                TableViewModel.TempShelterRequestDTOs.Clear();
+
+                var tempShelterRequests = GetFilteredTempShelterRequests();
+                var adoptionRequests = GetFilteredAdoptionRequests();
+                
+                if (tempShelterRequests != null)
+                {
+                    foreach (Domain.Model.TemporaryShelterRequest tempRequest in tempShelterRequests)
+                        TableViewModel.TempShelterRequestDTOs.Add(new TemporaryShelterRequestDTO(tempRequest));
+                }
+                else if(adoptionRequests != null)
+                {
+                    foreach (Domain.Model.AdoptionRequest adoptRequest in adoptionRequests)
+                        TableViewModel.AdoptionRequestDTOs.Add(new AdoptionRequestDTO(adoptRequest));
+                }
+                else
+                {
+                    MessageBox.Show("No requests found.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+        private void btnAccept_Click(object sender, EventArgs e)
+        {
+            //private void UpdateExam_Click(object sender, RoutedEventArgs e)
+
+            if (SelectedAnimal == null)
+            {
+                MessageBox.Show("Please choose animal to adopt!");
+            }
+            else
+            {
+                int animalId = SelectedAnimal.Id;
+                Post animalPost = null;
+                List<Post> posts = postController.GetAllPosts();
+                foreach (Post post in posts)
+                {
+                    if (post.AnimalId == animalId)
+                        animalPost = post;
+                }
+                if (animalPost.PostStatus == PostStatus.Adopted || animalPost.PostStatus == PostStatus.TemporarilyAdopted || animalPost.PostStatus == PostStatus.UnderTreatment)
+                {
+                    MessageBox.Show("Animal cannot be adopted!");
+                }
+                else
+                {
+                    MessageBox.Show("Request sent to volunteers!");
+                }
+            }
+
+            Close();
+        }
+       
+        private List<Domain.Model.TemporaryShelterRequest> GetFilteredTempShelterRequests()
+        {
+
+            List<Domain.Model.TemporaryShelterRequest> finalRequests = new List<Domain.Model.TemporaryShelterRequest>();
+
+            return finalRequests;
+        }
+        private List<Domain.Model.AdoptionRequest> GetFilteredAdoptionRequests()
+        {
+
+            List<Domain.Model.AdoptionRequest> finalRequests = new List<Domain.Model.AdoptionRequest>();
+
+            return finalRequests;
+        }
+    }
+}
